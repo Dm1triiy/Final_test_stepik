dfd3d9df960ff3f5ac05cad2e3dd589cb4e7574b
Dm1triiy <supermakarman@gmail.com>
2021-01-26 21:35:29 +0300

    testmainpg

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,8 @@
-import pytest
-import time
+from .pages.main_page import MainPage
 from selenium import webdriver
 
-def test_basket_find(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(link)
-    time.sleep(2)
-    input = browser.find_elements_by_css_selector("#add_to_basket_form>button")
-    assert input , "Basket not found"
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
